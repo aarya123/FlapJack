2beb957b00437da55ee57410b7de3151e2f68c1b
Vivian <vivian_hsiao@brown.edu>
2013-09-13 19:18:01 -0700

    Skeleton code for game logic

--- src/Engine/Hand.java
+++ src/Engine/Hand.java
@@ -1,69 +1,11 @@
-package Engine;
-
 import java.util.ArrayList;
-import java.util.List;
 
-/**
- * User: AnubhawArya
- * Date: 9/13/13
- * Time: 3:00 PM
- */
+
 public class Hand {
-    private List<Card> cards = new ArrayList<Card>();
-
-    // Constructor
-    public Hand(ArrayList<Card> cards) {
-        this.cards = cards;
-    }
-
-    // Called when player wants to hit
-    public void addCard(Card card) {
-        cards.add(card);
-    }
-
-    // Returns an array of length 1 or 2 containing possible values of the hand
-    public int[] getValues() {
-        int val0 = 0; // Hand value assuming first ace is valued at 1
-        int val1 = 0; // Hand value assuming first ace is valued at 11
-        boolean hasAce = false;
-        int[] cardValue;
-
-        for(int i=0; i<cards.size(); i++) {
-            cardValue = cards.get(i).getValues();
-
-            // If Ace
-            if(cardValue.length == 2) {
-                // If Ace already exists in hand, just add 1
-                if(hasAce) {
-                    val0 += cardValue[0]; // 1
-                    val1 += cardValue[0]; // 1
-                }
-                else {
-                    val0 += cardValue[0]; // 1
-                    val1 += cardValue[1]; // 11
-                    hasAce = true;
-                }
-            }
-
-            // Otherwise single-value cards (2-10, J, Q, K)
-            else {
-                val0 += cardValue[0];
-                val1 += cardValue[0];
-            }
-        }
-
-        if(val0 == val1) {
-            return new int[] { val0 };
-        }
-        else {
-            return new int[] {val0, val1};
-        }
-    }
-
-    // TODO
-   public Hand split() {
-	   return null;
-       // return Null unless cards.size() == 2;
-
-   }
+	Hand (ArrayList<Integer> cards) {;}
+	void addCard(Card card) {;}
+	ArrayList<Integer> getValues() {
+		return new ArrayList<Integer>(); //always length 1 or 2
+	}
+	
 }
