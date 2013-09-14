61a650aa3390054e84816173116b0dedadd43caf
Mackenzie Clark <mackenzie.a.clark@gmail.com>
2013-09-13 22:10:17 -0700

    fixed stuff

--- src/Engine/Session.java
+++ src/Engine/Session.java
@@ -14,10 +14,10 @@ public class Session {
     int numberOfWonGame;
 
 
-    public Session(Casino casino, Strategy strategy, Shoe shoe) {
+    public Session(Casino casino, Strategy strategy) {
         this.casino = casino;
         this.strategy = strategy;
-        this.shoe = shoe(7, strategy);
+        this.shoe = new Shoe(7, strategy);
 
         totalProfit = 0.0;
         finalPnl = 0.0;
--- src/Engine/Simulator.java
+++ src/Engine/Simulator.java
@@ -10,6 +10,6 @@ public class Simulator {
 
     public Simulator(Casino casino) {
         this.casino = casino;
-        new Session(casino, new Strategy(), new Shoe(1)).playGames();
+        new Session(casino, new Strategy()).playGames();
     }
 }
