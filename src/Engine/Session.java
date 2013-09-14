a5ecfc77e5a234358b0036c41dcac8a6041ea4d3
Anubhaw Arya <anubhaw.arya@gmail.com>
2013-09-14 10:52:12 -0700

    Removed methods

--- src/Engine/Session.java
+++ src/Engine/Session.java
@@ -37,11 +37,9 @@ public class Session {
 
             games[i] = new Game(strategy, casino, shoe, bet);
             games[i].play();
-Strategy.assess(games[i]);
             // shuffle deck when less than 25% remaining
             if ((double) shoe.size() / (double) shoeMax <= .25) {
                 shoe.shuffle();
-                Strategy.shuffled();
             }
         }
         solve();
--- src/Engine/Strategy.java
+++ src/Engine/Strategy.java
@@ -4,24 +4,55 @@ import java.util.HashMap;
 
 public class Strategy {
 
-    static HashMap<String, Double> hotnessMap;
-    static int deckHotness = 0;
+    static HashMap<String, Integer> hotnessMap;
 
     public Strategy() {
-        hotnessMap = new HashMap<String, Double>();
+        hotnessMap = new HashMap<String, Integer>();
         for (int i = 1; i < 14; i++) {
-            if (i == 1)
-                hotnessMap.put("A", -1.0);
-            else if (i == 11)
-                hotnessMap.put("J", -1.0);
-            else if (i == 12)
-                hotnessMap.put("Q", -1.0);
-            else if (i == 13)
-                hotnessMap.put("K", -1.0);
-            else if (i > 1 && i < 7)
-                hotnessMap.put(i + "", 1.0);
-            else
-                hotnessMap.put(i + "", 0.0);
+            switch (i) {
+                case 1:
+                    hotnessMap.put("A", -1);
+                    break;
+                case 2:
+                    hotnessMap.put(i + "", 1);
+                    break;
+                case 3:
+                    hotnessMap.put(i + "", 1);
+                    break;
+                case 4:
+                    hotnessMap.put(i + "", 2);
+                    break;
+                case 5:
+                    hotnessMap.put(i + "", 2);
+                    break;
+                case 6:
+                    hotnessMap.put(i + "", 2);
+                    break;
+                case 7:
+                    hotnessMap.put(i + "", 1);
+                    break;
+                case 8:
+                    hotnessMap.put(i + "", 0);
+                    break;
+                case 9:
+                    hotnessMap.put(i + "", 0);
+                    break;
+                case 10:
+                    hotnessMap.put(i + "", -2);
+                    break;
+                case 11:
+                    hotnessMap.put("J", -2);
+                    break;
+                case 12:
+                    hotnessMap.put("Q", -2);
+                    break;
+                case 13:
+                    hotnessMap.put("K", -2);
+                    break;
+                case 14:
+                    hotnessMap.put("A", -1);
+                    break;
+            }
         }
     }
 
@@ -30,25 +61,11 @@ public class Strategy {
     }
 
     double getBetMultiplier(double hotness) {
-        if (deckHotness < 25)
+        if (hotness < 25)
             return 1;
-        else if(deckHotness<30)
+        else if (hotness < 30)
             return 10;
         else
             return 15;
-    }
-
-    public static void shuffled() {
-        deckHotness = 0;
-    }
-
-    static int total = 0;
-
-    public static void assess(Game game) {
-        for (int i = 0; i < game.playerHands.size(); i++)
-            for (int j = 0; j < game.playerHands.get(i).getCards().size(); j++)
-                deckHotness += hotnessMap.get(game.playerHands.get(i).getCards().get(j).getRank());
-        deckHotness += hotnessMap.get(game.dealerHiddenCard.getRank());
-        total += deckHotness;
     }
 }
