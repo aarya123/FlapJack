e45af5dbdff7a12d9b6daf5519669313d399290d
Vivek Karuturi <Vivekxk@gmail.com>
2013-09-13 17:53:33 -0700

    asdf

--- src/Engine/Session.java
+++ src/Engine/Session.java
@@ -2,48 +2,92 @@
 
 public class Session {
 	
-	public Casino casino;
-	public Player player;
-	public Shoe shoe;
+	Casino casino;
+	Player player;
+	Shoe shoe;
+
 	final int numberOfGames = 5000;
-	public games[numberOfGames];
+
+	double totalProfit;
+	double finalPnl;
+	double totalWage;
+	double averageProfit;
+	double gameWonPercentage;
+
+	int numberOfWonGame;
+
 
 	public Session(Casin casino, Player player, Shoe shoe) {
 		this.casino = casino;
 		this.player = player;
 		this.shoe = shoe;
 
+		totalProfit = 0.0;
+		finalPnl = 0.0;
+		totalWage = 0.0;
+		averageProfit = 0.0;
+		gameWonPercentage = 0.0;
+		numberOfWonGame = 0;
+
 		games = new games[numberOfGames];
+		solve();
 	}
 
 	static game simulate() {
 		for (int i = 0; i < numberOfGames; i++) {
-			games[i]
+			//
 		}
 	}
 
-	Casino getCasino() {
+	public Casino getCasino() {
 		return casino;
 	}
 
-	Player getPlayer() {
+	public Player getPlayer() {
 		return player;
 	}
-	Shoe getShoe() {
+	public Shoe getShoe() {
 		return shoe;
 	}
 
-	void setCasino(Casino casino) {
+	public void setCasino(Casino casino) {
 		this.casino = casino;
 	}
-	void setPlayer(Player player) {
+	public void setPlayer(Player player) {
 		this.player = player;
 	}	
-	void setShoe(Shoe shoe) {
+	public void setShoe(Shoe shoe) {
 		this.shoe = shoe;
 	}
 
-	public static void main(String args[]) {
+	private void solve() {
+		for (int i = 0; i < numberOfGames; i++) {
+			finalPnl += games[i].getPnl; //???
+			totalProfit += games[i].getProfit; //??
+			totalWage += games[i].getWager;  //???
+			numberOfWonGame += totalProfit>0?1:0;
+		}
+	}
+
+	public double getAverageProfit() {
+		return double(finalPnl / totalWager);
+	}
+	public double getFinalPnl() {
+		return finalPnl;
+	}
+	public double getTotalWager() {
+		return totalWager;
+	}
+	public double getTotalProfit() {
+		return totalProfit;
+	}
+
+
+	public double getGameWonPercentage() {
+		return double(numberOfWonGame * 100 / numberOfGames);
+	}
+
+	public static void main(String args[]) {//just for testing.
 
 	} 
 }
