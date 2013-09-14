fe5182fb8ae086d7021bb635386fd196639cd46b
Jonathan <jonathan@upstart.com>
2013-09-13 19:56:42 -0700

    Add casino code from master

--- src/Engine/Casino.java
+++ src/Engine/Casino.java
@@ -1,4 +1,41 @@
+package Engine;
 
+/**
+ * User: AnubhawArya
+ * Date: 9/13/13
+ * Time: 3:21 PM
+ */
 public class Casino {
 
+    private int numberOfDecks;
+    private boolean hitOnSoft17s, doubleAfterSplit, resplitAfterAce;
+    private String name;
+
+    public Casino(String name, int numberOfDecks, boolean hitOnSoft17s, boolean doubleAfterSplit, boolean resplitAfterAce) {
+        this.name = name;
+        this.numberOfDecks = numberOfDecks;
+        this.hitOnSoft17s = hitOnSoft17s;
+        this.doubleAfterSplit = doubleAfterSplit;
+        this.resplitAfterAce = resplitAfterAce;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNumberOfDecks() {
+        return numberOfDecks;
+    }
+
+    public boolean isHitOnSoft17s() {
+        return hitOnSoft17s;
+    }
+
+    public boolean isDoubleAfterSplit() {
+        return doubleAfterSplit;
+    }
+
+    public boolean isResplitAfterAce() {
+        return resplitAfterAce;
+    }
 }
