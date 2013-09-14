bdf9a55543b45dbf9de40da9c445ca6147d95a5e
Jonathan <jonathan@upstart.com>
2013-09-14 00:22:48 -0700

    Do stuff

--- src/Engine/Strategy.java
+++ src/Engine/Strategy.java
@@ -4,10 +4,23 @@ public class Strategy {
 
     double[] hotnessMap, bettingFunction;
 
+    public Strategy() {
+      this.hotnessMap = randomHotnessMap(); // TODO
+      this.bettingFunction = randomBettingFunction(); // TODO
+    }
+
     // bettingFunction is an array of length 5; 
     public Strategy(double[] hotnessMap, double[] bettingFunction) {
       this.hotnessMap = hotnessMap;
       this.bettingFunction = bettingFunction;
+    }
+
+    double[] getHotnessMap {
+      return hotnessMap;
+    }
+
+    double[] getBettingFunction {
+      return bettingFunction;
     }
 
     double getHottnessForCard(Card card) {
@@ -17,4 +30,25 @@ public class Strategy {
     double getBetMultiplier(double hotness) {
       return 1; // TODO
     }
+
+    // Randomizer functions
+    
+    // Returns array mapping card values -> hotness values
+    // Indices are getValue()[0] - 1 (A -> 0, 2 -> 1, ... 10 -> 9)
+    double[] randomHotnessMap() {
+      Random r = new Random();
+
+      for(int i=0; i<10; i++) {
+        int randomSign = (Math.round(r.nextDouble()) == 1) ? 1 : -1; // random number: -1 or 1
+        double randomDouble = r.nextDouble() * 5; // random double between 0 and 5
+        randomHotnessMap[i] = randomDouble * randomSign; // random double between -5 and 5
+      }
+
+      return randomHotnessMap;
+    }
+
+    double[] randomBettingFunction() {
+      
+    }
+
 }
